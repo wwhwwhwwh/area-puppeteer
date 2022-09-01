db93eb00d31ef861812bb79f9e614c3025257895
wwhwwhwwh <819371938@qq.com>
2022-09-01 17:22:46 +0800

    format

--- src/utils.js
+++ src/utils.js
@@ -1,43 +1,40 @@
-const path = require('path');
-const fs = require('fs');
-const _config = { type: 'space', size: 4 }
+const path = require("path");
+const fs = require("fs");
+const _config = { type: "space", size: 4 };
 
-const JSONFormatter = require('json-format')
+const JSONFormatter = require("json-format");
 
 function timeout(delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            try {
-                resolve(1)
-            } catch (e) {
-                reject(0)
-            }
-        }, delay)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        resolve(1);
+      } catch (e) {
+        reject(0);
+      }
+    }, delay);
+  });
 }
-
-
 
 function writeFileModuleSync(name, data) {
-    let str = JSONFormatter(data, _config)
-    fs.writeFileSync(path.resolve(__dirname, name), `module.exports=${str}`);
+  let str = JSONFormatter(data, _config);
+  fs.writeFileSync(
+    path.resolve(__dirname, name),
+    `export default {\n ${str}  \n}`
+  );
 }
 
-
-
 function isEmptyObj(obj) {
-    return Object.keys(obj).length < 1
+  return Object.keys(obj).length < 1;
 }
 
 function mergeObject(obj1, ...param) {
-    return Object.assign(obj1, ...param)
+  return Object.assign(obj1, ...param);
 }
 
-
-
 module.exports = {
-    writeFileModuleSync: writeFileModuleSync,
-    mergeObject: mergeObject,
-    isEmptyObj: isEmptyObj,
-    timeout: timeout
+  writeFileModuleSync: writeFileModuleSync,
+  mergeObject: mergeObject,
+  isEmptyObj: isEmptyObj,
+  timeout: timeout,
 };
